a5e5b1b194e2327927ecda47fd1e582066da02d4
jasper-clarke <me@jasperclarke.com>
2025-02-16 18:37:45 +1100

    nixos unstable updated 16-Feb-2025

--- flake.nix
+++ flake.nix
@@ -2,33 +2,40 @@
   description = "Provides packages, modules and functions for the 06cb:009a fingerprint sensor.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs
-  }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-    localPackages = import ./pkgs/default.nix { pkgs = pkgs; };
-    localLib = import ./lib {
-      pkgs = pkgs;
-      localPackages = localPackages;
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      localPackages = import ./pkgs/default.nix { pkgs = pkgs; };
+      localLib = import ./lib {
+        pkgs = pkgs;
+        localPackages = localPackages;
+      };
+    in
+    {
+      nixosModules.python-validity = args:
+        import ./modules/python-validity (
+          args
+          // {
+            localPackages = localPackages;
+          }
+        );
+
+      nixosModules.open-fprintd = ./modules/open-fprintd;
+
+      nixosModules."06cb-009a-fingerprint-sensor" = args:
+        import ./modules/06cb-009a-fingerprint-sensor (
+          args
+          // {
+            localPackages = localPackages;
+            libfprint-2-tod1-vfs0090-bingch = localLib.libfprint-2-tod1-vfs0090-bingch;
+          }
+        );
     };
-  in {
-    nixosModules.python-validity = args: import ./modules/python-validity (
-      args // {
-        localPackages = localPackages;
-      }
-    );
-
-    nixosModules.open-fprintd = ./modules/open-fprintd;
-
-    nixosModules."06cb-009a-fingerprint-sensor" = args: import ./modules/06cb-009a-fingerprint-sensor (
-      args // {
-        localPackages = localPackages;
-        libfprint-2-tod1-vfs0090-bingch = localLib.libfprint-2-tod1-vfs0090-bingch;
-      }
-    );
-  };
 }
