d559bdc8c9b08aac5e0b00c7ddbe0b621944958f
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2025-03-22 03:42:57 +0100

    fix: add overlay for libfprint
    wainting until https://github.com/NixOS/nixpkgs/pull/389711 gets merged

--- flake.nix
+++ flake.nix
@@ -6,12 +6,30 @@
   };
 
   outputs =
-    { self
-    , nixpkgs
-    ,
+    {
+      self,
+      nixpkgs,
     }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      fixups = final: prev: {
+        libfprint-tod = prev.libfprint-tod.overrideAttrs (old: rec {
+          version = "1.90.7+git20210222+tod1";
+          src = old.src.overrideAttrs {
+            rev = "v${version}";
+            outputHash = "0cj7iy5799pchyzqqncpkhibkq012g3bdpn18pfb19nm43svhn4j";
+            outputHashAlgo = "sha256";
+          };
+          buildInputs = (old.buildInputs or [ ]) ++ [ final.nss ];
+          mesonFlags = [
+            "-Ddrivers=all"
+            "-Dudev_hwdb_dir=${placeholder "out"}/lib/udev/hwdb.d"
+          ];
+        });
+      };
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ fixups ];
+      };
       localPackages = import ./pkgs/default.nix { pkgs = pkgs; };
       localLib = import ./lib {
         pkgs = pkgs;
@@ -19,7 +37,8 @@
       };
     in
     {
-      nixosModules.python-validity = args:
+      nixosModules.python-validity =
+        args:
         import ./modules/python-validity (
           args
           // {
@@ -29,7 +48,8 @@
 
       nixosModules.open-fprintd = ./modules/open-fprintd;
 
-      nixosModules."06cb-009a-fingerprint-sensor" = args:
+      nixosModules."06cb-009a-fingerprint-sensor" =
+        args:
         import ./modules/06cb-009a-fingerprint-sensor (
           args
           // {
